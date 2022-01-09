62a2d7e68373b1ce7355092731d74c284299bf1a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-01-09 22:03:15 +0100

    cocktail shaker sort and its time complexities

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+void swap(listint_t **, listint_t **, listint_t **);
+/**
+  * cocktail_sort_list - cocktail sort algorithm for a dll
+  * @list: head of the list
+  * Return: nothing
+  */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *curr, *next;
+	int swapped;
+
+	curr = *list;
+	swapped = 0;
+	while (curr && curr->next)
+	{
+		next = curr->next;
+		if (curr->n > next->n)
+		{
+			swapped = 1;
+			swap(list, &curr, &next);
+			curr = next->next;
+		}
+		else
+			curr = next;
+	}
+
+	while (curr && curr->prev && swapped == 1)
+	{
+		next = curr->prev;
+		if (next->n > curr->n)
+		{
+			swapped = 1;
+			swap(list, &next, &curr);
+			curr = next->prev;
+		}
+		else
+			curr = next;
+	}
+	if (swapped == 0)
+		return;
+
+	cocktail_sort_list(list);
+}
+
+/**
+  * swap - swaps the positions of two items in a dll
+  * @head: head of the list
+  * @first: the first item
+  * @second: second item
+  * Return: nothing
+  */
+void swap(listint_t **head, listint_t **first, listint_t **second)
+{
+	if (*first && (*first)->prev)
+	{
+		(*first)->prev->next = *second;
+		(*second)->prev = (*first)->prev;
+		if ((*second)->next)
+			(*second)->next->prev = *first;
+		(*first)->next = (*second)->next;
+		(*first)->prev = *second;
+		(*second)->next = *first;
+		print_list(*head);
+	}
+	else if (*first)
+	{
+		(*first)->next = (*second)->next;
+		(*second)->next->prev = *first;
+		(*first)->prev = *second;
+		(*second)->prev = NULL;
+		(*second)->next = *first;
+		*head = *second;
+		print_list(*head);
+	}
+}
